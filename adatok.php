127836a1c515a8f30349bba1f584d2f490720076
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2025-09-15 16:35:36 +0200

    dgsdhgr

---
+++ adatok.php
@@ -0,0 +1,57 @@
+<?php
+    $adatok = [
+        "cim"=>[
+            "Budapest-Debrecen gyorsforgalmi útszakasz",
+            "Egy kis vonat",
+            "két katica",
+            "három kiskacsa",
+            "négy porszívó",
+            "öt póniló",
+            "hat pillangó",
+            "hét tudjha a g*ci",
+            "nyolc hinta",
+            "kilnec 11",
+            "tíz kiskutya",
+        ],
+        "kartya"=>[
+            "cim"=>[
+                "A kiskakas",
+                "A kis róka kalandjai",
+                "A bátor bagoly",
+                "Csillagfényes éjszaka",
+                "A titkos kert",
+                "Pöttyös Panni kalandja",
+                "A repülő teknős",
+                "Zöld erdő meséi",
+                "Tündérvarázs",
+                "A varázslatos tó",
+            ],
+            "szoveg"=>[
+                "Ebben az erdőben lakik egy kiskakas, aki minden reggel hangosan kukorékol.",
+                "A kis róka mindig kíváncsi volt a világra, és egy nap elindult felfedezni azt.",
+                "A bátor bagoly éjszakánként őrzi az erdőt, és segít a kis állatoknak.",
+                "Egy csillagfényes éjszakán a kis nyuszi elindult a kalandra.",
+                "A titkos kertben varázslatos virágok nyílnak, és minden nap új csodákat rejt.",
+                "Pöttyös Panni egy kis lány, aki mindig vidám és tele van energiával.",
+                "A repülő teknős egy különleges állat, aki szeret a felhők között szállni.",
+                "Zöld erdő meséi tele vannak izgalmas történetekkel és barátságokkal.",
+                "Tündérvarázs segít a gyerekeknek, hogy elaludjanak és szép álmokat lássanak.",
+                "A varázslatos tó mélyén különleges lények élnek, akik csak a bátraknak mutatkoznak meg.",
+
+            ],
+            "link"=>[
+                "#pornhub",
+                "#xvideos",
+                "#darknet",
+                "#deepweb",
+                "#freedarknet",
+
+                "#4chan",
+                "#8chan",
+                "#library",
+                "#archive",
+            ]
+        ]
+    ];
+
+?>
---
+++ weblapFeladat.php
@@ -0,0 +1,113 @@
+<?php
+    include("fugvenyek.php");
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Weblap Feladat</title>
+    <style>
+        #container {
+            border: 2px solid black;
+            border-radius: 10px;
+   
+            margin-top: 20px;
+            margin-bottom: 20px;
+           
+            background-color:rgb(197, 197, 197);
+
+        }
+        #kartya{
+            background-color: rgb(119, 152, 177);
+            border: 2px solid black;
+            border-radius: 10px;
+        }
+        #navbar{
+            border: 2px solid black;
+            border-radius: 10px;
+        }
+        
+    </style>
+</head>
+
+<body>
+
+<div class="container-lg" id="container">
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="display-1"><?php
+            echo $adatok["cim"][rand(0,sizeof($adatok["cim"])-1)];
+            ?></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4 p-2">
+            <div class="card w-100" id="kartya" >
+                <div class="card-body" >
+                    <h5 class="card-title"><?php
+                    echo $adatok["kartya"]["cim"][rand(0,sizeof($adatok["kartya"]["cim"])-1)];
+                    ?></h5>
+                    <p class="card-text"><?php
+                    echo $adatok["kartya"]["szoveg"][rand(0,sizeof($adatok["kartya"]["szoveg"])-1)];
+                    ?></p>
+                    <a href="<?php
+                    $adatok["kartya"]["link"][rand(0,sizeof($adatok["kartya"]["link"])-1)];
+                    ?>" class="btn btn-primary"><?php
+                    echo $adatok["kartya"]["link"][rand(0,sizeof($adatok["kartya"]["link"])-1)];
+                    ?></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8 p-2">
+            <nav class="navbar navbar-light bg-secondary" id="navbar">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1">Navbar</span>
+                </div>
+            </nav>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit.<img src="images.jpg" class=" rounded img-fluid float-end">Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+            
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4 justify-content-center p-2">
+            <div class="card h-100" id="kartya" >
+                <div class="card-body">
+                    <h5 class="card-title"><?php
+                    echo $adatok["kartya"]["cim"][rand(0,sizeof($adatok["kartya"]["cim"])-1)];
+                    ?></h5>
+                    <p class="card-text"><?php
+                    echo $adatok["kartya"]["szoveg"][rand(0,sizeof($adatok["kartya"]["szoveg"])-1)];
+                    ?></p>
+                    <a href="<?php
+                    $adatok["kartya"]["link"][rand(0,sizeof($adatok["kartya"]["link"])-1)];
+                    ?>" class="btn btn-primary"><?php
+                    echo $adatok["kartya"]["link"][rand(0,sizeof($adatok["kartya"]["link"])-1)];
+                    ?></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8 p-2">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<img src="images2.jpg" class="rounded img-fluid float-start"> Duis vulputate felis quis sollicitudin laoreet. Cras risus felis, sodales ultrices tellus a, aliquam semper dolor. Nunc ac tincidunt libero, ut rutrum elit. Cras tristique, justo eget pharetra finibus, diam leo finibus arcu, vel volutpat orci mauris sit amet massa. Etiam porta id ante et vestibulum. Etiam aliquam commodo lobortis. Mauris tincidunt viverra tristique. Nullam eget magna a lorem rutrum semper eget et dolor.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 p-2">
+            <footer class="bg-secondary text-white text-center">
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a.</p>
+            </footer>
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
