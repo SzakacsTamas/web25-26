71fd1cdaf9bdc49811bb30d38965237df7ecd97b
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2026-02-08 18:01:30 +0100

    dsgsdgh

---
+++ digitalisKultura/2024oktober/3_Autok_mozgasa/autok/autok/Program.cs
@@ -0,0 +1,16 @@
+﻿using autok;
+
+List<adatok> autok = new List<adatok>();
+string[] sorok = File.ReadAllLines("jeladas.txt");
+
+for (int i=0; i < sorok.Length; i++ )
+{
+    string[] adatok = sorok[i].Split("\t");
+    adatok adat=new adatok(adatok[0], int.Parse(adatok[1]), int.Parse(adatok[2]), int.Parse(adatok[3]));
+    autok.Add(adat);
+}
+
+foreach (adatok adat in autok)
+{
+    Console.WriteLine($"{adat.rendSzam} {adat.ora} {adat.perc} {adat.sebesseg}");
+}
---
+++ digitalisKultura/2024oktober/3_Autok_mozgasa/autok/autok/adatok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok
+{
+    internal class adatok
+    {
+        public string rendSzam;
+        public int ora;
+        public int perc;
+        public int sebesseg;
+
+        public adatok(string rendSzam, int ora, int perc, int sebesseg)
+        {
+            this.rendSzam= rendSzam;
+            this.ora= ora;
+            this.perc= perc;
+            this.sebesseg= sebesseg;
+
+        }
+    }
+}
