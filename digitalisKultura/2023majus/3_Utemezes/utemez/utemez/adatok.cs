e3418f92ff1647ce1ba39dfcb162cf67405912fc
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2026-02-18 10:40:51 +0100

    grgthz

---
+++ digitalisKultura/2023majus/3_Utemezes/utemez/utemez/adatok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utemez
+{
+    internal class adatok
+    {
+        public int kezdoHonap;
+        public int kezdoNap;
+        public int vegeHonap;
+        public int vegeNap;
+        public string diakAzonositok;
+        public string taborTipus;
+
+        public adatok(int kezdoHonap, int kezdoNap, int vegeHonap, int vegeNap, string diakAzonositok, string taborTipus)
+        {
+            this.kezdoHonap = kezdoHonap;
+            this.kezdoNap = kezdoNap;
+            this.vegeHonap = vegeHonap;
+            this.vegeNap = vegeNap;
+            this.diakAzonositok = diakAzonositok;
+            this.taborTipus = taborTipus;
+        }
+    }
+}
