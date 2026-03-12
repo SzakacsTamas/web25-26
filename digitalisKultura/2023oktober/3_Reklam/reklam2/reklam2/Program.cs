2bc2f50ac5aa8e059544259fc0c0793efd78d420
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2026-03-12 20:13:38 +0100

    dfsdfds

---
+++ digitalisKultura/2023oktober/3_Reklam/reklam2/reklam2/Program.cs
@@ -0,0 +1,69 @@
+﻿using reklam2;
+//1Feladat
+List<adatok> rendelesek = new List<adatok>();
+string[] sorok = File.ReadAllLines("rendel.txt");
+
+for (int i = 0; i < sorok.Length; i++)
+{
+    string[] adatok = sorok[i].Split(" ");
+    adatok adat = new adatok(int.Parse(adatok[0]), adatok[1], int.Parse(adatok[2]));
+    rendelesek.Add(adat);
+}
+
+//2Feladat
+Console.WriteLine("2.Feladat");
+int szam = 0;
+foreach(adatok adat in rendelesek)
+{
+    szam++;
+}
+Console.WriteLine($"A rendelések száma: {szam}");
+//LINKQ
+var rendelesSzam = rendelesek.Count();
+Console.WriteLine($"A rendelések száma: {rendelesSzam}");
+
+//3Feladat
+Console.WriteLine("3.Feladat");
+Console.Write("Kérem, adjon meg egy napot: ");
+int bekertNap = int.Parse(Console.ReadLine());
+int szam2 = 0;
+foreach(adatok adat in rendelesek)
+{
+    if(bekertNap == adat.nap)
+    {
+        szam2++;
+    }
+}
+Console.WriteLine($"A rendelések száma az adott napon: {szam2}");
+//LINKQ
+var rendelesSzamAdottNapon = rendelesek.Where(x => x.nap == bekertNap).Count();
+Console.WriteLine($"A rendelések száma az adott napon: {rendelesSzamAdottNapon}");
+
+//4Feladat
+Console.WriteLine("4.Feladat");
+//LINKQ
+var nemVoltRendeles = rendelesek.GroupBy(x => x.nap).Where(g => !g.Any(x => x.varos == "NR")).Count();
+Console.WriteLine($"{nemVoltRendeles} nap nem volt a reklámban nem érintett városból rendelés");
+
+//5Feladat
+Console.WriteLine("5.Feladat");
+var legnagyobb= rendelesek.Max(x =>x.rendelesSzama);
+var leganyobbNap = rendelesek.Where(x => x.rendelesSzama == legnagyobb).Select(x => x.nap).First();
+
+Console.WriteLine($"A legnagyobb darabszám: {legnagyobb}, a rendelés napja {leganyobbNap}");
+var adott = rendelesek.GroupBy(x => x.nap).Select(g => new
+{
+    Nap = g.Key,
+    OsszesRendeles = g.Sum(x => x.rendelesSzama) // Rendeles mező összeadása
+})
+    .ToList();
+
+foreach (var a in adott)
+{
+    Console.WriteLine(a.OsszesRendeles);
+}
+//6Feladat
+void osszes()
+{
+
+}
---
+++ digitalisKultura/2023oktober/3_Reklam/reklam2/reklam2/adatok.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reklam2
+{
+    internal class adatok
+    {
+        public int nap;
+        public string varos;
+        public int rendelesSzama;
+
+        public adatok(int nap, string varos, int rendelesSzama)
+        {
+            this.nap = nap;
+            this.varos = varos;
+            this.rendelesSzama = rendelesSzama;
+        }
+    }
+}
